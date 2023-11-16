9356e4c52eee7192adde2b37272785bf88521022
MariannePV <mariannepulgarvega12@gmail.com>
2023-11-16 16:50:54 +0100

    canvis tipografia

--- Assets/Scripts/MiniGame/CartController.cs
+++ Assets/Scripts/MiniGame/CartController.cs
@@ -1,27 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEditor.Progress;
 
 public class CartController : MonoBehaviour
 {
+    //Canvas elements
+    [SerializeField]
+    TextMeshProUGUI marcador;
+    [SerializeField]
+    TextMeshProUGUI timerText;
+    [SerializeField]
+    GameObject panelFinal;
+    [SerializeField]
+    GameObject panelMsgFinal;
+    [SerializeField]
+    Image bananaImg;
+    [SerializeField]
+    TextMeshProUGUI msgFinalWin;
+    [SerializeField]
+    Image duckImg;
+    [SerializeField]
+    TextMeshProUGUI msgFinalOver;
+    [SerializeField]
+    GameObject btnTornar;
+    [SerializeField]
+    GameObject btnReiniciar;
+
     private Rigidbody2D cart;   //Jugador
     public Camera mainCamera;
     public float margin = 0.1f; //Marge per controlar la distància amb les cantonades (equerra i dreta)
     private bool edgeRight = false;
     private bool edgeLeft = false;
     public float speed;    //Veloccitat amb la que es mou el jugador
-    internal int puntuacioTotal;
+    public int puntuacioTotal;
     public GameObject collectableBanana;
     public GameObject collectableDuck;
 
-    private double timerCollectable = 1f;
-    private double timerCollectableDuck = 1.3f;
+    private double timerCollectable;
+    private double timerCollectableDuck;
+
+    //Les variables per crear el timer
+    float secondsCount = 0;
+
+    public float totalTime = 30.0f; // Total time in seconds
+
+    private bool timerZero = false;
 
     // Start is called before the first frame update
     void Start()
     {
         cart = GetComponent<Rigidbody2D>();
+        secondsCount = totalTime;
     }
 
     private void FixedUpdate()
@@ -42,6 +74,8 @@ public class CartController : MonoBehaviour
 
     void Update()
     {
+        UpdateTimerUI();
+
         timerCollectable -= Time.deltaTime;
         timerCollectableDuck -= Time.deltaTime;
 
@@ -68,15 +102,57 @@ public class CartController : MonoBehaviour
         if (timerCollectable <= 0f)    //Collectable Banana
         {
             Instantiate(collectableBanana, new Vector3(Random.Range(-8, 8), 10 + transform.position.y, 0), Quaternion.identity);     //Instanciem el col·leccionable
-            timerCollectable = 1f;
-            //Debug.Log(puntuacioTotal);
+            timerCollectable = 0.8f;
         }
 
         if (timerCollectableDuck <= 0f)    //Collectable Duck
         {
             Instantiate(collectableDuck, new Vector3(Random.Range(-8, 8), 10 + transform.position.y, 0), Quaternion.identity);     //Instanciem el col·leccionable
-            timerCollectableDuck = 1.3f;
-            //Debug.Log(puntuacioTotal);
+            timerCollectableDuck = 1.1f;
         }
+
+        marcador.text = puntuacioTotal.ToString();
+        
+        //El joc para quan et quedes sense temps o quan aconsegueixes 15 plàtans
+        if (timerZero)  //S'ha acabat el temps
+        {
+            Time.timeScale = 0;
+            panelFinal.SetActive(true);
+            panelMsgFinal.SetActive(true);
+            duckImg.gameObject.SetActive(true);
+            msgFinalOver.gameObject.SetActive(true);
+            btnReiniciar.gameObject.SetActive(true);
+        }
+
+        if (puntuacioTotal == 15 && !timerZero)   //Has acabat el joc (15 punts)
+        {
+            Time.timeScale = 0;
+            panelFinal.SetActive(true);
+            panelMsgFinal.SetActive(true);
+            bananaImg.gameObject.SetActive(true);
+            msgFinalWin.gameObject.SetActive(true);
+            btnTornar.gameObject.SetActive(true);
+        }
+    }
+
+    public void calcularPuntuacio(int punt) //Funció per sumar punts (els punts li pasen els collectables des del script CollectableController.cs)
+    {
+        puntuacioTotal = puntuacioTotal + punt;
+    }
+
+    public void UpdateTimerUI() //Es tracta d'una funció per afegir un comptador al videojoc
+    {
+        secondsCount -= Time.deltaTime;
+
+        if (secondsCount <= 0)
+        {
+            secondsCount = 0;
+            // Handle timer reaching zero (e.g., end the game or perform some action)
+            timerZero = true;
+        }
+
+        int seconds = Mathf.FloorToInt(secondsCount);
+
+        timerText.text = string.Format("{00:00}", seconds);
     }
 }
--- Assets/Scripts/MiniGame/CollectableContoller.cs
+++ Assets/Scripts/MiniGame/CollectableContoller.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class CollectableContoller : MonoBehaviour
 {
-    private int puntuacio;
+    private CartController cartController;
 
     // Start is called before the first frame update
     void Start()
     {
-        //CartController cartController = GetComponent<CartController>();
-
-        //cartController.puntuacioTotal = puntuacio;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        cartController = GameObject.FindObjectOfType<CartController>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -28,17 +20,16 @@ public class CollectableContoller : MonoBehaviour
         }
 
         if (collision.tag == "Player" && collision.tag != "CartLeft" && collision.tag != "CartRight")
-        {
-            Destroy(gameObject);
+        {            
             if (gameObject.tag == "Banana")
-            {                
-                puntuacio++;
+            {
+                cartController.calcularPuntuacio(1);
             } else if (gameObject.tag == "Duck")
             {
-                puntuacio--;
+                cartController.calcularPuntuacio(-1);
             }
 
-            Debug.Log(puntuacio);
+            Destroy(gameObject);
         }
     }    
 }
